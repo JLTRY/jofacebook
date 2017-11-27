ae04b1581c3df531e41d912a8cf742fd4bc2a51a
tryoen <tryoen>
2017-11-27 18:34:41 +0000

    git-svn-id: svn://localhost/joomlaplugins/trunk@15 963b6780-89e0-4a4c-83fb-b9431da22b1d

---
+++ content/fbkarticle/editor-xtd/fbkarticle.php
@@ -0,0 +1,35 @@
+<?php
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.plugin.plugin' );
+
+class plgButtonFbkArticle extends JPlugin {
+
+	
+	function onDisplay($name)
+	{
+		$js =  "function sampleFbkArticleClick(editor,id) {
+			txt = prompt('Please enter ID of article','123');
+			if (!txt) return;
+			jInsertEditorText('{fbkarticle id='+id+';article='+txt+'}', editor);
+		}";
+		$params = $this->params;	
+		
+		$id = $params->get('id','?');
+		
+		$doc = JFactory::getDocument();
+		$doc->addScriptDeclaration($js);
+		
+		$button = new JObject;		
+		$button->modal = false;
+		$button->class = 'btn';
+		$button->link = '#';
+		$button->text = JText::_('Insert Facebook article');
+		$button->name = 'fbk';
+		$button->onclick = 'sampleFbkArticleClick(\''.$name. '\',' . $id . '); return false;';
+		
+		return $button;
+	}
+}
